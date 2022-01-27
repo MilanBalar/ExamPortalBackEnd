0eea75e222e01b21b1282d6d6ac675d5770a6cef
MilanBalar <milanbalar2000@gmail.com>
2022-01-27 10:33:10 +0530

    step:5 handle the all types of exception & send the exception message to angular

--- ExamPortalServer/src/main/java/com/boot/server/exam/contollers/UserController.java
+++ ExamPortalServer/src/main/java/com/boot/server/exam/contollers/UserController.java
@@ -4,9 +4,11 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.boot.server.exam.exception.UserFoundException;
 import com.boot.server.exam.models.TblRoles;
 import com.boot.server.exam.models.TblUser;
 import com.boot.server.exam.models.TblUsersRole;
@@ -61,10 +64,20 @@ public class UserController {
         tblUsersRole.setTblRoles(roles);
 
         tblUsersRoles.add(tblUsersRole);
+      return this.userService.createUser(tblUser, tblUsersRoles);
+    }
 
-
-        return this.userService.createUser(tblUser, tblUsersRoles);
-
+	//UserFoundException Handling
+	@ExceptionHandler(UserFoundException.class)
+	public ResponseEntity<?> exceptionHandler(UserFoundException e){
+		System.out.println("inside the UserFoundException handler class");
+		return ResponseEntity.badRequest().body(e.getMessage());
 	}
 
+	//Exception Handling
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<?> exceptionHandler(Exception e) {
+		System.out.println("inside the Exception handler class");
+		return ResponseEntity.badRequest().body(e.getMessage());
+	}
 }
